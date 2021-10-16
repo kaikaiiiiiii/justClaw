ae7044ed79c1d7bfa68a517f8eb76da9e3d9d92c
kaikai <yourkaikai@gmail.com>
2021-10-16 21:46:25 +0800

    update

--- xboxGold-freegames-all-regions.js
+++ xboxGold-freegames-all-regions.js
@@ -7,8 +7,6 @@ const _ = require('lodash');
 const sanitize = require("sanitize-filename");
 
 
-
-
 const regions = [
     { code: "zh-HK", name: "香港/澳門特別行政區 - 繁體中文" },
     { code: "zh-TW", name: "台灣 - 繁體中文" },
@@ -55,89 +53,90 @@ const regions = [
     { code: "sk-SK", name: "Slovensko - Slovenčina" },
     { code: "sv-SE", name: "Sverige - Svenska" },
     { code: "tr-TR", name: "Türkiye - Türkçe" },
-    // China mainland xbox gold has no free games, 
-    //don't uncommet else it will stuck to overtime and exit;
-    //{ code: "zh-CN", name: "中国 - 中文" }, 
+    // China mainland xbox gold has no free games, don't uncommet it
+    // { code: "zh-CN", name: "中国 - 中文" }, 
 ];
 
 
 var spider = async (regions) => {
-  var results = [];
-  const browser = await chromium.launch({
-    //headless: false,
-  })
-  const page = await browser.newPage()
-  await page.setViewportSize({ width: 1200, height: 1000 });
-  page.route('**/*', (route) => {
-    if (route.request().resourceType() == 'image') { route.abort()} else { route.continue()};
-  });
-  console.log('Robot started.')
-  for (let i = 0; i < regions.length; i++) {
-    let url = `https://www.xbox.com/${regions[i].code}/live/gold#gameswithgold`;
-    console.log(`${i+1}\/${regions.length+1} visiting ${regions[i].code}: ${regions[i].name}`)
-    await page.goto(url, {timeout:0});
-    await page.waitForSelector('#ContentBlockList_9 section a', {timeout:600*1000});
-    var contents = await page.$$eval('#ContentBlockList_9 section a', els => {
-      return els.map(el => {
-        var link = el.getAttribute('href');
-        var img = el.querySelector('img').getAttribute('src');
-        var date = el.querySelector('span.availDate').innerText;
-        var title = el.querySelector('h3.c-heading').innerText;
-        return { title, date, img, link};
-      })
+    var results = [];
+    const browser = await chromium.launch({
+        //headless: false,
+    })
+    const page = await browser.newPage()
+    await page.setViewportSize({ width: 1200, height: 1000 });
+    page.route('**/*', (route) => {
+        if (route.request().resourceType() == 'image') { route.abort() } else { route.continue() };
     });
-    contents.forEach(e => {
-      e.rName = regions[i].name;
-      e.rCode = regions[i].code;
-    });
-    results = results.concat(contents);
-  }
-  await page.close();
-  await browser.close();
-  return results
+    console.log('Robot started.')
+    for (let i = 0; i < regions.length; i++) {
+        let url = `https://www.xbox.com/${regions[i].code}/live/gold#gameswithgold`;
+        console.log(`${i + 1}\/${regions.length} visiting ${regions[i].code}: ${regions[i].name}`)
+        await page.goto(url, { timeout: 0 });
+        await page.waitForSelector('#ContentBlockList_9 section a', { timeout: 600 * 1000 });
+        var contents = await page.$$eval('#ContentBlockList_9 section a', els => {
+            return els.map(el => {
+                var storeLink = el.getAttribute('href');
+                var img = el.querySelector('img').getAttribute('src');
+                var availDate = el.querySelector('span.availDate').innerText;
+                var title = el.querySelector('h3.c-heading').innerText;
+                return { title, availDate, img, storeLink };
+            })
+        });
+        contents.forEach(e => {
+            e.rName = regions[i].name;
+            e.rCode = regions[i].code;
+            e.source = url;
+        });
+        results = results.concat(contents);
+    }
+    await page.close();
+    await browser.close();
+    return results
 }
 
-async function downloadImage(imageFilename,imageURL) {
-  const url = imageURL;
-  const fpath = path.resolve(__dirname, imageFilename)
-  const writer = fs.createWriteStream(fpath)
+async function downloadImage(imageFilename, imageURL) {
+    const url = imageURL;
+    const fpath = path.resolve(__dirname, imageFilename)
+    const writer = fs.createWriteStream(fpath);
 
-  const response = await axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
+    const response = await axios({
+        url,
+        method: 'GET',
+        responseType: 'stream'
+    });
 
-  response.data.pipe(writer)
+    response.data.pipe(writer)
 
-  return new Promise((resolve, reject) => {
-    writer.on('finish', resolve)
-    writer.on('error', reject)
-  })
+    return new Promise((resolve, reject) => {
+        writer.on('finish', resolve)
+        writer.on('error', reject)
+    })
 }
 
 async function main() {
-  console.log(`Job started.`)
-  var data = await spider(regions);
-  var udata = _.uniqBy(data, 'title');
-  var ucsv = papa.unparse(udata, {
-    header: true,
-    newline: '\r\n',
-    columns:['title','rCode','date','rName','link','img']
-  });
-  var filename = 'AllRegionXboxGoldGames.csv'
-  if (fs.existsSync(filename)) { fs.unlinkSync(filename) };
-  fs.writeFileSync(filename, ucsv, 'utf-8');
+    console.log(`Job started.`)
+    var data = await spider(regions);
+    var udata = _.uniqBy(data, 'title');
+    var ucsv = papa.unparse(udata, {
+        header: true,
+        newline: '\r\n',
+        delimiter: ',\t',
+        columns: ['availDate', 'rCode', 'title', 'storeLink'] //, 'rName', 'source'
+    });
+    var filename = 'AllRegionXboxGoldGames.csv'
+    if (fs.existsSync(filename)) { fs.unlinkSync(filename) };
+    fs.writeFileSync(filename, ucsv, 'utf-8');
 
-  console.log(`uniqued data saved, downloading cover image`)
-  for (let i = 0; i < udata.length; i++){
-    var item = udata[i];
-    var filename = 'A.' + sanitize(item.title) + '.jpg';
-    var imgurl = item.img;
-    await downloadImage(filename,imgurl)
-  }
-  
-  console.log('All jobs done.');
+    console.log(`writing data to csv and downloading cover images`)
+    for (let i = 0; i < udata.length; i++) {
+        var item = udata[i];
+        var filename = 'A.' + sanitize(item.title) + '.jpg';
+        var imgurl = item.img;
+        await downloadImage(filename, imgurl)
+    }
+
+    console.log('All jobs done.');
 }
 
 main();
