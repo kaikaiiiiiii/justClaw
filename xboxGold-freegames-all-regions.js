d03700910ba16877c8039f2088757ad4341320c4
kaikaiiiiiii <yourkaikai@gmail.com>
2021-11-17 17:57:38 +0800

    update

--- xboxGold-freegames-all-regions.js
+++ xboxGold-freegames-all-regions.js
@@ -1,4 +1,4 @@
-const { chromium } = require('playwright');
+const { chromium } = require('playwright-chromium');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
