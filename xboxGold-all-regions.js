5704c1e8b7e3a75208ba1c6931dbff34dc976c7f
kaikai <yourkaikai@gmail.com>
2022-01-03 19:27:07 +0800

    update

--- xboxGold-all-regions.js
+++ xboxGold-all-regions.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const papa = require('papaparse');
-const _ = require('lodash');
 const sanitize = require("sanitize-filename");
 
 
@@ -13,46 +12,46 @@ const regions = [
     { code: "zh-TW", name: "台灣 - 繁體中文" },
     { code: "en-HK", name: "Hong Kong/Macau (SAR) - English" },
     { code: "ja-JP", name: "日本 - 日本語" },
-    { code: "ko-KR", name: "대한민국 - 한국어" },
-    { code: "en-AU", name: "Australia - English" },
-    { code: "en-CA", name: "Canada - English" },
-    { code: "en-GB", name: "United Kingdom - English" },
-    { code: "en-IE", name: "Ireland - English" },
-    { code: "en-IN", name: "India - English" },
-    { code: "en-NZ", name: "New Zealand - English" },
-    { code: "en-SG", name: "Singapore - English" },
-    { code: "en-ZA", name: "South Africa - English" },
-    { code: "fr-BE", name: "Belgique - Français" },
-    { code: "fr-CA", name: "Canada - Français" },
-    { code: "fr-CH", name: "Suisse - Français" },
-    { code: "fr-FR", name: "France - Français" },
-    { code: "de-AT", name: "Österreich - Deutsch" },
-    { code: "de-CH", name: "Schweiz - Deutsch" },
-    { code: "de-DE", name: "Deutschland - Deutsch" },
-    { code: "es-AR", name: "Argentina - Español" },
-    { code: "es-CL", name: "Chile - Español" },
-    { code: "es-CO", name: "Colombia - Español" },
-    { code: "es-ES", name: "España - Español" },
-    { code: "es-MX", name: "México - Español" },
-    { code: "it-IT", name: "Italia - Italiano" },
-    { code: "ar-AE", name: "الإمارات العربية المتحدة - العربية" },
-    { code: "ar-SA", name: "المملكة العربية السعودية - العربية" },
-    { code: "cs-CZ", name: "Česká Republika - Čeština" },
-    { code: "da-DK", name: "Danmark - Dansk" },
-    { code: "el-GR", name: "Ελλάδα - Ελληνικά" },
-    { code: "fi-FI", name: "Suomi - Suomi" },
-    { code: "he-IL", name: "ישראל - עברית" },
-    { code: "hu-HU", name: "Magyarország - Magyar" },
-    { code: "nb-NO", name: "Norge - Norsk bokmål" },
-    { code: "nl-BE", name: "België - Nederlands" },
-    { code: "nl-NL", name: "Nederland - Nederlands" },
-    { code: "pl-PL", name: "Polska - Polski" },
-    { code: "pt-BR", name: "Brasil - Português" },
-    { code: "pt-PT", name: "Portugal - Português" },
-    { code: "ru-RU", name: "Россия - Русский" },
-    { code: "sk-SK", name: "Slovensko - Slovenčina" },
-    { code: "sv-SE", name: "Sverige - Svenska" },
-    { code: "tr-TR", name: "Türkiye - Türkçe" },
+    // { code: "ko-KR", name: "대한민국 - 한국어" },
+    // { code: "en-AU", name: "Australia - English" },
+    // { code: "en-CA", name: "Canada - English" },
+    // { code: "en-GB", name: "United Kingdom - English" },
+    // { code: "en-IE", name: "Ireland - English" },
+    // { code: "en-IN", name: "India - English" },
+    // { code: "en-NZ", name: "New Zealand - English" },
+    // { code: "en-SG", name: "Singapore - English" },
+    // { code: "en-ZA", name: "South Africa - English" },
+    // { code: "fr-BE", name: "Belgique - Français" },
+    // { code: "fr-CA", name: "Canada - Français" },
+    // { code: "fr-CH", name: "Suisse - Français" },
+    // { code: "fr-FR", name: "France - Français" },
+    // { code: "de-AT", name: "Österreich - Deutsch" },
+    // { code: "de-CH", name: "Schweiz - Deutsch" },
+    // { code: "de-DE", name: "Deutschland - Deutsch" },
+    // { code: "es-AR", name: "Argentina - Español" },
+    // { code: "es-CL", name: "Chile - Español" },
+    // { code: "es-CO", name: "Colombia - Español" },
+    // { code: "es-ES", name: "España - Español" },
+    // { code: "es-MX", name: "México - Español" },
+    // { code: "it-IT", name: "Italia - Italiano" },
+    // { code: "ar-AE", name: "الإمارات العربية المتحدة - العربية" },
+    // { code: "ar-SA", name: "المملكة العربية السعودية - العربية" },
+    // { code: "cs-CZ", name: "Česká Republika - Čeština" },
+    // { code: "da-DK", name: "Danmark - Dansk" },
+    // { code: "el-GR", name: "Ελλάδα - Ελληνικά" },
+    // { code: "fi-FI", name: "Suomi - Suomi" },
+    // { code: "he-IL", name: "ישראל - עברית" },
+    // { code: "hu-HU", name: "Magyarország - Magyar" },
+    // { code: "nb-NO", name: "Norge - Norsk bokmål" },
+    // { code: "nl-BE", name: "België - Nederlands" },
+    // { code: "nl-NL", name: "Nederland - Nederlands" },
+    // { code: "pl-PL", name: "Polska - Polski" },
+    // { code: "pt-BR", name: "Brasil - Português" },
+    // { code: "pt-PT", name: "Portugal - Português" },
+    // { code: "ru-RU", name: "Россия - Русский" },
+    // { code: "sk-SK", name: "Slovensko - Slovenčina" },
+    // { code: "sv-SE", name: "Sverige - Svenska" },
+    // { code: "tr-TR", name: "Türkiye - Türkçe" },
     // China mainland xbox gold has no free games, don't uncommet it
     // { code: "zh-CN", name: "中国 - 中文" }, 
 ];
@@ -100,6 +99,7 @@ var spider = async (regions) => {
 }
 
 async function downloadImage(imageFilename, imageURL) {
+    if (fs.existsSync(imageFilename)) { return };
     const url = imageURL;
     const fpath = path.resolve(__dirname, imageFilename)
     const writer = fs.createWriteStream(fpath);
@@ -136,6 +136,24 @@ function readCSV(f) {
     });
 }
 
+function shrinkBy(arr, key) {
+    var o = {};
+    arr.forEach(e => {
+        if (!o[e[key]]) {
+            o[e[key]] = e;
+        } else {
+            o[e[key]].rCode = o[e[key]].rCode + ',' + e.rCode;
+        }
+    });
+    return Object.values(o);
+}
+
+var papaOutputFormat = {
+    header: true,
+    newline: '\r\n',
+    delimiter: ',\t',
+    columns: ['title', 'availDate', 'storeLink', 'rCode'] //, 'rName', 'source'
+}
 
 async function main() {
     console.log(`Job started.`)
@@ -143,24 +161,14 @@ async function main() {
     var data = await spider(regions);
     var old_data = await readCSV(filename);
     var alldata = data.concat(old_data);
-    var udata = _.uniqBy(data, 'title');
-    var ucsv = papa.unparse(udata, {
-        header: true,
-        newline: '\r\n',
-        delimiter: ',\t',
-        columns: ['availDate', 'rCode', 'title', 'storeLink'] //, 'rName', 'source'
-    });
+    var udata = shrinkBy(data, 'title');
+    var ucsv = papa.unparse(udata, papaOutputFormat);
 
-    var ualldata = _.uniqBy(alldata, 'title');
-    var uallcsv = papa.unparse(ualldata, {
-        header: true,
-        newline: '\r\n',
-        delimiter: ',\t',
-        columns: ['availDate', 'rCode', 'title', 'storeLink'] //, 'rName', 'source'
-    });
+    var ualldata = shrinkBy(alldata, 'title');
+    var uallcsv = papa.unparse(ualldata, papaOutputFormat);
 
     console.log('==============================');
-    console.log(ucsv);
+    console.log(ucsv.replaceAll('\t', '\r\n'));
     console.log('==============================');
     if (fs.existsSync(filename)) { fs.unlinkSync(filename) };
     fs.appendFileSync(filename, uallcsv, 'utf-8');
@@ -168,7 +176,7 @@ async function main() {
     console.log(`writing data to csv and downloading cover images`);
     for (let i = 0; i < udata.length; i++) {
         var item = ualldata[i];
-        var filename = sanitize('A.' + item.code + '.' + item.title + '.' + item.availDate + '.jpg');
+        var filename = sanitize('A.' + item.rCode.split(',')[0] + '.' + item.title + '.' + item.availDate.match(/\d{1,2}\/\d{1,2}/g)[0].replaceAll('\/', '-') + '.jpg');
         var imgurl = item.img;
         if (imgurl == undefined) { continue } else {
             await downloadImage(filename, imgurl)
