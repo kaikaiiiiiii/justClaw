388cf13f021cd4b0689ab3def35d4869baa86c29
kaikaiiiiiii <yourkaikai@gmail.com>
2022-11-04 15:06:30 +0800

    update

--- wowhead-recipe.js
+++ wowhead-recipe.js
@@ -8,7 +8,16 @@ const sanitize = require("sanitize-filename");
 
 const recipes = [
     "https://www.wowhead.com/wotlk/cn/enchanting",
-    "https://www.wowhead.com/wotlk/cn/enchanting"
+    "https://www.wowhead.com/wotlk/cn/jewelcrafting",
+    "https://www.wowhead.com/wotlk/cn/engineering",
+    "https://www.wowhead.com/wotlk/cn/tailoring",
+    "https://www.wowhead.com/wotlk/cn/blacksmithing",
+    "https://www.wowhead.com/wotlk/cn/alchemy",
+    "https://www.wowhead.com/wotlk/cn/leatherworking",
+    "https://www.wowhead.com/wotlk/cn/cooking",
+    "https://www.wowhead.com/wotlk/cn/inscription",
+    "https://www.wowhead.com/wotlk/cn/mining",
+    "https://www.wowhead.com/wotlk/cn/first-aid"
 ];
 
 
@@ -19,12 +28,11 @@ var getRecipeList = async (page) => {
 
 var getSpell = async (itemid) => {
     var content = [{
-        category: '附魔',
-        name: '配方',
+        category: 'enchanting',
+        name: '附魔护腕 - 特效耐力',
         type: 'spell',
         link: 'https://www.wowhead.com/wotlk/cn/spell=62256/%E9%99%84%E9%AD%94%E6%8A%A4%E8%85%95-%E7%89%B9%E6%95%88%E8%80%90%E5%8A%9B',
-        material: [{ name: "强效宇宙精化", id: "34055", number: 4 }, { name: "深渊水晶" },],
-        source: "商人",
+        recipe: [{ name: "强效宇宙精化", id: "34055", qty: 4 }, { name: "深渊水晶", id: "34057", qty: 1 },],
         skill: [450, 460, 470, 480]
     }]
     return content;
@@ -47,9 +55,9 @@ async function main() {
     const page = await browser.newPage()
     const disabledTypes = ['image', 'font', 'stylesheet'];
     await page.setViewportSize({ width: 1200, height: 1000 });
-    page.route('**/*', (route) => {
-        if (disabledTypes.includes(route.request().resourceType())) { route.abort() } else { route.continue() };
-    });
+    // page.route('**/*', (route) => {
+    //     if (disabledTypes.includes(route.request().resourceType())) { route.abort() } else { route.continue() };
+    // });
 
     var itemlist = [];
     var spelllist = [];
